16cea3d0ae5cb180384d174f3d7d9ee92bdc3949
Loden <loden.gendun@students.mq.edu.au>
2022-05-07 19:08:39 +1000

    Cell Values Hidden Until Clicked - Removed innerHTML assignment on cellDisplay creation. - Added on click event handlers to all cells. - Clicking cell registers click with Business Logic, refreshes boardData and updates cell with value.

--- scripts/DOM-handler.js
+++ scripts/DOM-handler.js
@@ -9,18 +9,36 @@ const createCell = (x, y) => {
     cell.classList.add('cell');
     cell.setAttribute('data-xpos', x);
     cell.setAttribute('data-ypos', y);
-    cell.innerText = board[y][x].display;
     return cell;
 }
 
 window.addEventListener('DOMContentLoaded', () => {
     // Find the 'board' node
-    const board = document.querySelector('.board');
-    
+    const boardDisplay = document.querySelector('.board');
+    // Set up grid
     for (let y = 0; y < gridSize; y++) {
         for(let x = 0; x < gridSize; x++) {
             // Create & append new cell to the board
-            board.appendChild(createCell(x, y));
+            boardDisplay.appendChild(createCell(x, y));
         }
     }
+
+    // Set on click listeners for each cell
+    const cellNodes = document.querySelectorAll('.cell');
+    cellNodes.forEach(cellDisplay => {
+        cellDisplay.addEventListener('click', (event) => {
+            const cellNode = event.target;
+            const x = cellNode.attributes['data-xpos'].value;
+            const y = cellNode.attributes['data-ypos'].value;
+            
+            // Register click with Business Logic
+            const clickOutcome = game.clickCell([y, x]);
+            // Update board state
+            board = game.board;
+            // Fetch cell data 
+            const cellData = board[y][x];
+            // Check visibility of cell
+            if (!cellData.hidden) cellNode.innerHTML = cellData.display;
+        })
+    })
 });
